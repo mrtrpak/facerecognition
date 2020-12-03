7ce2a845602cbbf05b4fe6bc3fe106263f682527
Michael Trpak <mrtrpak3@gmail.com>
2020-12-03 18:32:23 -0500

    brain image shows on page

--- src/components/Logo/script.js
+++ src/components/Logo/script.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 
-import Photo from '../../assets/brainLogo.png';
+import photo from '../../assets/brainLogo.png';
 import './style.css';
 
 export default function Logo() {
@@ -9,7 +9,7 @@ export default function Logo() {
     <div className='ma4 mt0'>
       <Tilt className='Tilt br3 shadow-3' options={{ max: 70 }} style={{ height: 200, width: 200 }}>
         <div className='Tilt-inner'>
-          <Photo />
+          <img src={photo} alt='Brain' />
         </div>
       </Tilt>
     </div>
