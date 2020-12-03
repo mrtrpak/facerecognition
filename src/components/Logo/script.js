e34002a13e979c71aee03865f1906b115478df55
Michael Trpak <mrtrpak3@gmail.com>
2020-12-03 18:11:43 -0500

    setting styling for logo box

--- src/components/Logo/script.js
+++ src/components/Logo/script.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 
+import './style.css';
+
 export default function Logo() {
   return (
     <div className='ma4 mt0'>
-      <Tilt className='Tilt' options={{ max: 25 }} style={{ height: 250, width: 250 }}>
+      <Tilt className='Tilt br3 shadow-3' options={{ max: 70 }} style={{ height: 200, width: 200 }}>
         <div className='Tilt-inner'>
           👽
         </div>
