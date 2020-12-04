88279f5d06addd3315b7759456c1e19ed24ed678
Michael Trpak <mrtrpak3@gmail.com>
2020-12-04 15:44:09 -0500

    aligning the logo in the center of the box using CSS flexbox

--- src/components/Logo/script.js
+++ src/components/Logo/script.js
@@ -8,7 +8,7 @@ export default function Logo() {
   return (
     <div className='ma4 mt0'>
       <Tilt className='Tilt br3 shadow-3' options={{ max: 70 }} style={{ height: 200, width: 200 }}>
-        <div className='Tilt-inner'>
+        <div className='Tilt-inner pa3'>
           <img src={photo} alt='Brain' />
         </div>
       </Tilt>
