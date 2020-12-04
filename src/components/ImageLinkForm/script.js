ff7018e9a1a90fa55bbfc4014fda6b5d05716148
Michael Trpak <mrtrpak3@gmail.com>
2020-12-04 16:11:01 -0500

    adding in stylesheet for comp

--- src/components/ImageLinkForm/script.js
+++ src/components/ImageLinkForm/script.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './style.css';
+
 export default function ImageLinkForm() {
   return (
     <div>
@@ -7,8 +9,10 @@ export default function ImageLinkForm() {
         { 'This Brain will detect faces in pictures! Give it a try.'}
       </p>
       <div className='centerAlign'>
-        <input className='f4 pa2 w-80 center' type='text' />
-        <button className='w30 grow f4 link ph3 pv2 dib white bg-light-blue'>Detect</button>
+        <div className='pa3 br3 shadow-3'>
+          <input className='f4 pa2 w-80 center' type='text' />
+          <button className='w-20 grow f4 link ph3 pv2 dib white bg-light-blue'>Detect</button>
+        </div>
       </div>
     </div>
   );
