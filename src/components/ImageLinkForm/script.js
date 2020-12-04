377f1adabc9eba592f5351e481aba6e76aae7651
Michael Trpak <mrtrpak3@gmail.com>
2020-12-04 15:52:44 -0500

    adding app.css file and added styling to imagelink comp

--- src/components/ImageLinkForm/script.js
+++ src/components/ImageLinkForm/script.js
@@ -3,7 +3,13 @@ import React from 'react';
 export default function ImageLinkForm() {
   return (
     <div>
-      link form
+      <p className='f3'>
+        { 'This Brain will detect faces in pictures! Give it a try.'}
+      </p>
+      <div className=''>
+        <input className='f4 pa2 w-70 center' type='text' />
+        <button className='w30 grow f4 link ph3 pv2 dib white bg-light-green'>Detect</button>
+      </div>
     </div>
   );
 };
