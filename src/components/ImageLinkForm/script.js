1ea871db12263f08f688941a4c05a581205e4e42
Michael Trpak <mrtrpak3@gmail.com>
2020-12-04 16:29:52 -0500

    using global classes for 2 different backgrounds and adding to necessary class names

--- src/components/ImageLinkForm/script.js
+++ src/components/ImageLinkForm/script.js
@@ -9,7 +9,7 @@ export default function ImageLinkForm() {
         { 'This Brain will detect faces in pictures! Give it a try.'}
       </p>
       <div className='centerAlign'>
-        <div className='form centerAlign pa3 br3 shadow-3'>
+        <div className='form centerAlign minorBackground pa3 br3 shadow-3'>
           <input className='f4 pa2 w-80 center' type='text' />
           <button className='w-20 grow f4 link ph3 pv2 dib white bg-light-blue'>Detect</button>
         </div>
