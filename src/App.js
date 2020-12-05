c3e7659e2bd14ecac4529c7c7ab21f2e972b2434
Michael Trpak <mrtrpak3@gmail.com>
2020-12-04 21:16:48 -0500

    adding in react-particles-js and putting into app comp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import ImageLinkForm from './components/ImageLinkForm/script';
 import Logo from './components/Logo/script';
 import Navigation from './components/Navigation/script';
@@ -10,6 +11,19 @@ import './App.css';
 export default function App() {
   return (
     <div>
+      <Particles 
+        params={{
+          particles: {
+            line_linked: {
+              shadow: {
+                enable: true,
+                color: '#3CA9D1',
+                blur: 3
+              }
+            }
+          }
+        }} 
+      />
       <Navigation />
       <Logo />
       <Rank />
