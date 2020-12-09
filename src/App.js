e7a21a87fddd533916910b7aa7a3af989b8e5c3a
Michael Trpak <mrtrpak3@gmail.com>
2020-12-09 18:49:02 -0500

    adding state to App

--- src/App.js
+++ src/App.js
@@ -23,6 +23,17 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = event => {
+    console.log(event);
+  };
+
   render() {
     return (
       <div>
@@ -30,7 +41,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} />
       </div>
     );
   };
