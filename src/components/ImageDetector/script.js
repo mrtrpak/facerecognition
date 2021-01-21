1bf4a719baa675d2ec75b4b1a19566b12c69420c
Michael Trpak <mrtrpak3@gmail.com>
2021-01-21 18:11:47 -0500

    adding styling to component

--- src/components/ImageDetector/script.js
+++ src/components/ImageDetector/script.js
@@ -4,7 +4,7 @@ import './style.css';
 
 const ImageDetector = () => {
   return (
-    <div className='center'>
+    <div className='centerAlign'>
       <img src={'https://thumbs.dreamstime.com/b/composite-multiple-female-faces-looking-camera-smiling-169175698.jpg'} alt="none showing" />
     </div>
   );
