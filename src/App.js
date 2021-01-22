de1a7b56e99cb115d11be6cf96f6632e0bccaa40
Michael Trpak <mrtrpak3@gmail.com>
2021-01-22 16:53:03 -0500

    getting api face detection data

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
     app.models.predict( 
       Clarifai.FACE_DETECT_MODEL, this.state.input).then(
         function(response) {
-          console.log(response);
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
         },
         function(err) {
           throw err;
