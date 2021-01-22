acb5614cdb5e8728d978cfda9c31e47ae1a59a8b
Michael Trpak <mrtrpak3@gmail.com>
2021-01-22 16:22:59 -0500

    getting the data response from the api

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
     app.models.predict( 
       Clarifai.COLOR_MODEL, this.state.input).then(
         function(response) {
-          console.log(response);
+          console.log(response.outputs[0].data);
         },
         function(err) {
           throw err;
