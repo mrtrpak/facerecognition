fe331c126b548c09d16017e829bc89899a4f6f3e
Michael Trpak <mrtrpak3@gmail.com>
2021-01-28 17:52:06 -0500

    adding more styling

--- src/components/Logo/script.js
+++ src/components/Logo/script.js
@@ -8,7 +8,7 @@ import photo from '../../assets/brainLogo.png';
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
-      <Tilt className='Tilt br3 shadow-3 centerAlign mainBackground' options={{ max: 70 }} style={{ height: 200, width: 200 }}>
+      <Tilt className='Tilt br3 shadow-3 mainBackground' options={{ max: 70 }} style={{ height: 200, width: 200 }}>
         <div className='Tilt-inner pa3'>
           <img src={photo} alt='Brain' />
         </div>
