f88b8483f4974d4d5f70f393b5cf7d78a905d5e5
Michael Trpak <mrtrpak3@gmail.com>
2021-01-28 17:51:32 -0500

    redid the styling

--- src/components/ImageLinkForm/script.js
+++ src/components/ImageLinkForm/script.js
@@ -5,13 +5,13 @@ import './style.css';
 const ImageLinkForm = ({ onInputChange, onSubmit }) => {
   return (
     <div>
-      <p className='f3 centerAlign'>
+      <p className='f3'>
         { 'This Brain will detect faces in pictures! Give it a try.'}
       </p>
-      <div className='centerAlign'>
-        <div className='form centerAlign minorBackground pa3 br3 shadow-3'>
+      <div className='center'>
+        <div className='form center minorBackground pa3 br3 shadow-5'>
           <input className='f4 pa2 w-80 center' type='text' onChange={onInputChange} />
-          <button className='w-20 grow f4 link ph3 pv2 dib white bg-light-blue' onClick={onSubmit}>Detect</button>
+          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue' onClick={onSubmit}>Detect</button>
         </div>
       </div>
     </div>
