de54dd79b02d4c8cfb6cd1bc88c8ad763e8cddd4
Michael Trpak <mrtrpak3@gmail.com>
2021-02-02 17:26:30 -0500

    setting up register comp

--- src/components/Register/script.js
+++ src/components/Register/script.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
-const Register = () => {
+const Register = ({ onRouteChange }) => {
   return (
-    <div>
-      Register
-    </div>
+    <article className="br3 ba dar-gray b--black-10 mv4 w-75-l w-50-m w-25-1 shadow-3 center">
+      <main className="pa4 black-80">
+        <form className="measure">
+          <fieldset id="register" className="ba b--transparent ph0 mh0">
+            <legend className="f1 fw6 ph0 mh0">Register</legend>
+            <div className="mt3">
+              <label className="db fw6 1h-copy f6"  htmlFor="email-address">Email</label>
+              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 1h-copy f6" htmlFor="password">Password</label>
+              <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
+            </div>
+          </fieldset>
+          <div>
+            <input 
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+              type="submit" 
+              value="Sign In"
+              onClick={onRouteChange}
+            />
+          </div>
+          <div className="1h-copy mt3">
+            <a href="#0" className="f6 link dim black db">Login</a>
+          </div>
+        </form>
+      </main>
+    </article>
   );
 };
 
