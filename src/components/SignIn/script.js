e3f7d47d366998d452bd3630387143572a12c086
Michael Trpak <mrtrpak3@gmail.com>
2021-02-02 15:26:33 -0500

    setting the sign in styling and adding border

--- src/components/SignIn/script.js
+++ src/components/SignIn/script.js
@@ -4,29 +4,29 @@ import './style.css';
 
 const SignIn = () => {
   return (
-    <main className="pa4 black-80">
-      <form className="measure center">
-        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-          <legend className="f4 fw6 ph0 mh0">Sign In</legend>
-          <div className="mt3">
-            <label className="db fw6 1h-copy f6" for="email-address">Email</label>
-            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
+    <article className="br2 ba dar-gray b--black-10 mv4 w-100 w-50-m w-25-1 mw5 center">
+      <main className="pa4 black-80">
+        <form className="measure">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+            <div className="mt3">
+              <label className="db fw6 1h-copy f6" for="email-address">Email</label>
+              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 1h-copy f6" for="password">Password</label>
+              <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
+            </div>
+          </fieldset>
+          <div>
+            <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign In" />
           </div>
-          <div className="mv3">
-            <label className="db fw6 1h-copy f6" for="password">Password</label>
-            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
+          <div className="1h-copy mt3">
+            <a href="#0" className="f6 link dim black db">Sign Up</a>
           </div>
-          <label className="pa0 ma0 1h-copy f6 pointer"><input type="checkbox" /> Remember Me </label>
-        </fieldset>
-        <div>
-          <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign In" />
-        </div>
-        <div className="1h-copy mt3">
-          <a href="#0" className="f6 link dim black db">Sign Up</a>
-          <a href="#0" className="f6 link dim black db">Forgot Password?</a>
-        </div>
-      </form>
-    </main>
+        </form>
+      </main>
+    </article>
   );
 };
 
