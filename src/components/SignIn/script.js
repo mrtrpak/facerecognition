df550d7e348839f017a025dae51d5a6aceaa05ff
Michael Trpak <mrtrpak3@gmail.com>
2021-02-02 15:29:44 -0500

    adding extra styling

--- src/components/SignIn/script.js
+++ src/components/SignIn/script.js
@@ -4,11 +4,11 @@ import './style.css';
 
 const SignIn = () => {
   return (
-    <article className="br2 ba dar-gray b--black-10 mv4 w-100 w-50-m w-25-1 mw5 center">
+    <article className="br3 ba dar-gray b--black-10 mv4 w-100 w-50-m w-25-1 shadow-3 center">
       <main className="pa4 black-80">
         <form className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
               <label className="db fw6 1h-copy f6" for="email-address">Email</label>
               <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
@@ -22,7 +22,7 @@ const SignIn = () => {
             <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign In" />
           </div>
           <div className="1h-copy mt3">
-            <a href="#0" className="f6 link dim black db">Sign Up</a>
+            <a href="#0" className="f6 link dim black db">Register</a>
           </div>
         </form>
       </main>
