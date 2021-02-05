0b6b9e17a051d49fd737bbbde2d2865e5c06178f
Michael Trpak <mrtrpak3@gmail.com>
2021-02-05 15:26:37 -0500

    setting up logic for nav comp to update if the user is signed in or not

--- src/components/Navigation/script.js
+++ src/components/Navigation/script.js
@@ -1,16 +1,39 @@
 import React from 'react';
 
-const Navigation = ({ onRouteChange }) => {
-  return (
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+  if (isSignedIn) {
+    return (
     <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-      <p 
-        className='f3 link dim black underline pa3 pointer'
-        onClick={() => onRouteChange('signIn')}
-      >
-        Sign Out
-      </p>
-    </nav>
-  );
+        <p 
+          className='f3 link dim black underline pa3 pointer'
+          onClick={() => onRouteChange('signIn')}
+          >
+          Sign Out
+        </p>
+      </nav>
+    );
+    } else {
+      return (
+        <div>
+          <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <p 
+            className='f3 link dim black underline pa3 pointer'
+            onClick={() => onRouteChange('signIn')}
+            >
+              Sign In
+            </p>
+          </nav>
+          <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <p 
+              className='f3 link dim black underline pa3 pointer'
+              onClick={() => onRouteChange('signIn')}
+              >
+              Register
+            </p>
+        </nav>
+      </div>
+    );
+  }
 };
 
 export default Navigation;
