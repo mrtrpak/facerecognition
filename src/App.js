eda17662c4c28a0b221b3da7837088bd88510504
Michael Trpak <mrtrpak3@gmail.com>
2021-02-21 18:29:08 -0500

    updating clarifai model

--- src/App.js
+++ src/App.js
@@ -95,7 +95,18 @@ class App extends Component {
       .predict( 
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if (response) {
+          fetch('https://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
   };
 
