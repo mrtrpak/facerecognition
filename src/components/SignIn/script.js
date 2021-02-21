ecb125bb5c47d507a9f00b4ccfe1f22ac54f3ece
Michael Trpak <mrtrpak3@gmail.com>
2021-02-21 17:45:27 -0500

    passing user info using props

--- src/components/SignIn/script.js
+++ src/components/SignIn/script.js
@@ -29,11 +29,14 @@ class SignIn extends Component {
       })
     })
     .then(response => response.json())
-    .then(data => {
-      if (data === 'success') {
-        this.props.onRouteChange('home');
-      };
-    });
+    .then(
+      user => {
+        if(user.id) {
+          this.props.loadUser(user);
+          this.props.onRouteChange('home');
+        };
+      }
+    );
   };
 
   render() {
