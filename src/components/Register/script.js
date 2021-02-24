0076bab68843dda72e1061161da0a2f5a7e4c06c
Michael Trpak <mrtrpak3@gmail.com>
2021-02-24 17:42:09 -0500

    updates

--- src/components/Register/script.js
+++ src/components/Register/script.js
@@ -23,7 +23,7 @@ class Register extends Component {
   };
 
   onSubmitRegister = () => {
-    fetch('https://localhost:3000/register', {
+    fetch('https://localhost:3001/register', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
