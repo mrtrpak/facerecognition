cf9f5fe9429792b0cfc8866dc59328fcba8d2747
Michael Trpak <mrtrpak3@gmail.com>
2021-03-11 17:13:01 -0500

    updating link route

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://pacific-wildwood-77993.herokuapp.com:3001/image', {
+          fetch('https://pacific-wildwood-77993.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
